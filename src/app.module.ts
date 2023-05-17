830a107b4a267f9f618537c190f94e264579c642
Elena Bless <beles02@bk.ru>
2023-05-17 17:36:47 +0300

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-// import {ConfigModule, ConfigService} from "@nestjs/config";
-// import {TypeOrmModule} from "@nestjs/typeorm";
-//
-// import {PostEntity} from "@/posts/entities/post.entity";
-//
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+import {PostEntity} from "@/posts/entities/post.entity";
+
 import { PostsModule } from './posts/posts.module';
 import { FriendsModule } from './friends/friends.module';
 import { ProfilesModule } from './profiles/profiles.module';
@@ -15,22 +15,22 @@ import { AuthorizationModule } from './authorization/authorization.module';
 
 @Module({
   imports: [
-      // ConfigModule.forRoot(),
-      // TypeOrmModule.forRootAsync({
-      //     imports: [ConfigModule],
-      //     inject: [ConfigService],
-      //     useFactory: (configService: ConfigService) => ({
-      //         type: 'postgres',
-      //         host: configService.get<string>('POSTGRES_HOST'),
-      //         port: configService.get<number>('POSTGRES_PORT'),
-      //         username: configService.get<string>('POSTGRES_USERNAME'),
-      //         password: configService.get<string>('POSTGRES_PASSWORD'),
-      //         database: configService.get<string>('POSTGRES_DATABASE'),
-      //         entities: [PostEntity],
-      //         synchronize: true,
-      //         autoLoadEntities: true
-      //     })
-      // }),
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => ({
+              type: 'postgres',
+              host: configService.get<string>('POSTGRES_HOST'),
+              port: configService.get<number>('POSTGRES_PORT'),
+              username: configService.get<string>('POSTGRES_USERNAME'),
+              password: configService.get<string>('POSTGRES_PASSWORD'),
+              database: configService.get<string>('POSTGRES_DATABASE'),
+              entities: [PostEntity],
+              synchronize: true,
+              autoLoadEntities: true
+          })
+      }),
       PostsModule,
       FriendsModule,
       ProfilesModule,
